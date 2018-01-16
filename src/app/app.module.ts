bd42faa673077e67db9855ddc9c5cffb1b658004
Leonardo Adell <leoadell@gmail.com>
2018-01-16 16:15:31 -0300

    new producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
