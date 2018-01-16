1139cbbd425dd7489b8178555d94834e1d2296a8
Leonardo Adell <leoadell@gmail.com>
2018-01-16 18:07:39 -0300

    producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,8 @@ import { CurrencyPipe } from '@angular/common/src/pipes/number_pipe';
 })
 
 export class AppComponent {
-  public name: string;
-  public detail: string;
-  public picture: string;
-  public precioVenta: number;
-  public precioCompra: number;
-  public cantidad: number;
 
   title = 'app works!';
 
-  constructor() {
-    this.cantidad = 20;
-    this.detail = "hilo negro carretel chico de 10mts marca cadena";
-    this.name = "Hilo negro";
-    this.precioCompra = 12;
-    this.precioVenta = 20.50;
-  }
+  constructor() {  }
 }
--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-producto',
+  selector: 'producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  public name: string;
+  public detail: string;
+  public foto: string;
+  public precioVenta: number;
+  public precioCompra: number;
+  public cantidad: number;
+
+  constructor() {
+    this.cantidad = 20;
+    this.detail = "hilo negro carretel chico de 10mts marca cadena";
+    this.name = "Hilo negro";
+    this.precioCompra = 12;
+    this.precioVenta = 20;
+  }
 
   ngOnInit() {
   }
