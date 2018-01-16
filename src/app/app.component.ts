41c3a29ff87f2ae7e6f3e2061ec63452dd679954
Leonardo Adell <leoadell@gmail.com>
2018-01-16 17:58:02 -0300

    producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common/src/pipes/number_pipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  public name: string;
+  public detail: string;
+  public picture: string;
+  public precioVenta: number;
+  public precioCompra: number;
+  public cantidad: number;
+
   title = 'app works!';
+
+  constructor() {
+    this.cantidad = 20;
+    this.detail = "hilo negro carretel chico de 10mts marca cadena";
+    this.name = "Hilo negro";
+    this.precioCompra = 12;
+    this.precioVenta = 20.50;
+  }
 }
