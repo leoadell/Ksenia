eac489e7507d5e10aeacd5ccaa1ef865d20da4c4
Leonardo Adell <ladell@belatrix.com>
2018-01-17 18:37:09 -0300

    service worker

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    navigateFallback: '/index.html',
+    stripPrefix: 'dist',
+    root: 'dist/',
+    staticFileGlobs: [
+      'dist/index.html',
+      'dist/**.js',
+      'dist/**.css',
+      'dist/assets/**.svg',
+      'dist/assets/**.png',
+      'dist/assets/icons/**.png',
+      'dist/assets/**.jpg',
+      'https://fonts.googleapis.com/icon?family=Material+Icons'
+    ]
+  };
