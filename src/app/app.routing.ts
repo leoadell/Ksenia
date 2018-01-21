2ed4ed8c4a8e3b9118cb7f6f5f5782dc855c48ab
Leonardo Adell <leoadell@gmail.com>
2018-01-21 12:34:01 -0300

    routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,17 @@
-import{ModuleWithProviders} from  '@angular/core';
-import {Routes, RouterModule }from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 //Components
-import{ProductoComponent} from './producto/producto.component';
+import { ProductoComponent } from './producto/producto.component';
+import { HomeComponent } from './home/home.component';
+import { AppModule } from 'app/app.module';
 
-const appRoutes: Routes=[
-    {path: '', component: ProductoComponent},
-]
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'producto', component: ProductoComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: ProductoComponent }
+];
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
